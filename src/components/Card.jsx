cbbd532445b1561b3069e7d34604ac304a7df0c6
Cyril Freeman <cyril.freeman@hotmail.fr>
2023-05-11 18:03:58 +0200

    rendu esthétique amélioré

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
 export default function Card(item) {
   const [isFavorited, setIsFavorited] = useState(false);
   const description = item.description;
@@ -14,32 +14,50 @@ export default function Card(item) {
       <img src={description.img} alt={description.descriptions} />
       <div className="card-details">
         <h2>{description.descriptions}</h2>
-        <div className="card-info
-">
-          <p>Durée {description.temps}</p>
-          <p>Niveau {description.niveau}</p>
-          <p>A patir de {description.prix}</p>
+        <div
+          className="card-info
+"
+        >
+          <p>
+            Durée
+            <br /> {description.temps}
+          </p>
+          <p>
+            Niveau
+            <br /> {description.niveau}
+          </p>
+          <p clasName="prix">A partir de {description.prix}</p>
         </div>
-        <div className="card-dates
-">
-          <div className="card-date
-  ">
+        <div
+          className="card-dates
+"
+        >
+          <div
+            className="card-date
+  "
+          >
             <p>{description.date1}</p>
             <p>{description.place1}</p>
           </div>
-          <div className="card-date
-  ">
+          <div
+            className="card-date
+  "
+          >
             <p>{description.date2}</p>
             <p>{description.place2}</p>
           </div>
-          <div className="card-date
-  ">
+          <div
+            className="card-date
+  "
+          >
             <p>{description.date3}</p>
             <p>{description.place3}</p>
           </div>
         </div>
-        <div className="card-buttons
-">
+        <div
+          className="card-buttons
+"
+        >
           <button onClick={handleLetsGo}>Let’s go !</button>
           <button
             type="button"
@@ -47,7 +65,8 @@ export default function Card(item) {
             onClick={toggleFavorite}
           >
             <span className="heartIcon">
-              {isFavorited ? ":cœur:" : ":coeur_blanc:"}</span>
+              {isFavorited ? ":cœur:" : ":coeur_blanc:"}
+            </span>
           </button>
         </div>
       </div>
