453b11044f03454be0ed6aeac5c48f75d34fff52
elea641 <eleavoliotis64@gmail.com>
2023-05-11 15:27:33 +0200

    cardList

--- src/components/CardList.jsx
+++ src/components/CardList.jsx
@@ -7,9 +7,12 @@ const CardList = () => {
 
   return (
     <div className='container-cardlist'>
-        {description.map((item, index) => (
-            <Card key={index} description={item} />
-        ))}
+        <h2>Les incontournables</h2>
+        <div className='container-cardlist-img'>
+            {description.map((item, index) => (
+                <Card key={index} description={item} />
+            ))}
+        </div>
     </div>
   );
 };
--- src/pages/LocalisationPage.jsx
+++ src/pages/LocalisationPage.jsx
@@ -1,18 +1,25 @@
 import React from "react";
 import { useParams } from 'react-router-dom';
 import { Localisations } from "../datas/Localisations";
+import Card from "../components/Card";
+import { Description } from '../data/description';
 
 const LocalisationPage = () => {
     const { id } = useParams();
     const localisation = Localisations.find(localisation => localisation.id === parseInt(id));
-  
-    console.log(localisation)
+    const description = Description.filter(description => description.localisation === localisation.localisation);
 
     return (
-      <div className="container-LocalisationPage">
-        <div className="container-LocalisationPage-img">
-        <h2>{localisation.localisation}</h2>
-        <img src={localisation.url} alt={localisation.localisation} />
+      <div className="container-Location">
+        <div className="container-LocalisationPage">
+          <div className="container-LocalisationPage-img">
+          <h2>{localisation.localisation}</h2>
+          <img src={localisation.url} alt={localisation.localisation} />
+          </div>
+          <div className="container-card-list-theme">
+            {description.map((item, index) => 
+              <Card key={index} description={item}/>)}
+          </div>
         </div>
       </div>
     );
